5b1ddc620b3fcabb097294b7bc50a8b76074bcc3
hujieqing@hotmail.com <xiaoyiou@gmail.com>
2020-03-04 17:27:55 -0800

    add basic preprocessing and visulization

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,31 @@
-'''
-This is the module to load graph data using pyTorch Geometrics
-'''
+import os.path as osp
+
 import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+from torch_geometric.nn import SplineConv
+from torch_geometric.utils.convert import to_networkx
+import matplotlib.pyplot as plt
+import networkx as nx
+
+dataset = 'Cora'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
+dataset = Planetoid(path, dataset, T.TargetIndegree())
+data = dataset[0]
+
+data.train_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+data.train_mask[:data.num_nodes - 1000] = 1
+data.val_mask = None
+data.test_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+data.test_mask[data.num_nodes - 500:] = 1\
+
+cora = dataset[0]
+
+coragraph = to_networkx(cora)
+
+node_labels = cora.y[list(coragraph.nodes)].numpy()
+
+plt.figure(1,figsize=(14,12)) 
+nx.draw(coragraph, cmap=plt.get_cmap('Set1'),node_color = node_labels,node_size=75,linewidths=6)
+plt.show()
